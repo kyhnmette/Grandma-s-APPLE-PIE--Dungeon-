e7e9b64916c9f34326e710bf64ed6cba3aef4938
Steffen Pihl <spihl12@student.aau.dk>
2014-03-08 15:33:36 +0100

    Updated Unity project
    Added the "A" and "D" move functions into the move hero script.
    Made a Prefab foulder for all our models (My wall is in there for now)
    Created a parent class called "Humaniod".
    And created a child class of Humaniod called "Hero"
    Have tried to experiment with adding objects into the scene with code,
    this have NOT been successful jet but the scrips are still there.

--- nyt unity project/Assets/Scripts/moveing_the_hero.cs
+++ nyt unity project/Assets/Scripts/moveing_the_hero.cs
@@ -10,6 +10,9 @@ public class moveing_the_hero : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown (KeyCode.A))
+			transform.position -= new Vector3(3, 0, 0);	// Lets the hero move to the left
+		if(Input.GetKeyDown (KeyCode.D))
+			transform.position += new Vector3(3, 0, 0);	// Lets the hero move to the right
 	}
 }
