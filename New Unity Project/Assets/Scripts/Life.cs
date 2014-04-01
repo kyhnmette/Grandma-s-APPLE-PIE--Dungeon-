958f86dd99bb046469b862aa247ed2b66cce123b
Steffen Pihl <spihl12@student.aau.dk>
2014-04-01 13:27:22 +0200

    Added Life scrip

---
+++ New Unity Project/Assets/Scripts/Life.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+	
+	protected short HP {get; set;}
+	protected short ATK {get; set;}
+	protected short Speed {get; set;}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (HP <= 0)
+			Destroy(gameObject);
+	}
+	
+	public void Attack () {
+		
+	}
+	
+	public void GetHit (short DmgTaken) {
+		HP =- DmgTaken;
+	}
+}
