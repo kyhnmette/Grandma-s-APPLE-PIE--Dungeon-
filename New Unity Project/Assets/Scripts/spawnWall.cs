aa39989f316d4021d8a76ef11eec98fd230f83d9
LisbethRasmussen <llra12@student.aau.dk>
2014-04-01 12:19:51 +0200

    Wallspawning updated
    A real wall added, although the wall needs a nice to have graphic
    adjustment when talking about the spawning corners.

--- New Unity Project/Assets/Scripts/spawnWall.cs
+++ New Unity Project/Assets/Scripts/spawnWall.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class spawnWall : MonoBehaviour {
 	
-    public GameObject myCube;
+    public GameObject myCubeDOWN;
+	public GameObject myCubeUP;
 	public GameObject TheFloar;
  
     void Start() { //remember to change the vector coordinates according to the size of the object.
 		
-		for (float i = 0; i<30; i++){
-       GameObject cubeSpawn = (GameObject)Instantiate(myCube, new Vector3(i,0.5f,0), transform.rotation);
+		for (float i = 0; i<30; i+=2){
+       GameObject cubeSpawn = (GameObject)Instantiate(myCubeUP, new Vector3(i,1f,0), transform.rotation);
 		} //cubespawn: giving each object a name or it will not work
 		//instantiate the gameobject
 		//myCube telling it what it has to spawn
 		//new vector3 telling it where to put it (z,y,x) (left/right, up/down, forth/back)
 		// transform.rotation, the instantiate needs to know where the object faces towards, even if we do not change the original rotation.
 		
-		for (float i = 0; i<30; i++){
-       	GameObject cubeSpawn = (GameObject)Instantiate(myCube, new Vector3(0,0.5f,i), transform.rotation);
+		for (float i = 0; i<30; i+=2){
+       	GameObject cubeSpawn = (GameObject)Instantiate(myCubeDOWN, new Vector3(0,1f,i), transform.rotation);
 		}
-				for (float i = 0; i<30; i++){
-       	GameObject cubeSpawn = (GameObject)Instantiate(myCube, new Vector3(29f,0.5f,i), transform.rotation);
+				for (float i = 0; i<30; i+=2){
+       	GameObject cubeSpawn = (GameObject)Instantiate(myCubeDOWN, new Vector3(29f,1f,i), transform.rotation);
 		}
-				for (float i = 0; i<30; i++){
-       	GameObject cubeSpawn = (GameObject)Instantiate(myCube, new Vector3(i,0.5f,29f), transform.rotation);
+				for (float i = 0; i<30; i+=2){
+       	GameObject cubeSpawn = (GameObject)Instantiate(myCubeUP, new Vector3(i,1f,29f), transform.rotation);
 		}
 		
 		GameObject floarSpawn = (GameObject)Instantiate(TheFloar, new Vector3(14.5f,-0.5f,14.5f), transform.rotation);
