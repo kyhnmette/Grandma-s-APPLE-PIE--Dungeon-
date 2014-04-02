d311421ebdec90b3f1cab56a4464c213867b2514
Steffen Pihl <spihl12@student.aau.dk>
2014-04-02 12:16:37 +0200

    Added Ball of Fire script

---
+++ New Unity Project/Assets/Scripts/BallOfFire.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallOfFire : MonoBehaviour {
+	
+	private bool isHit {get; set;}
+	private short BallDmg = 40;
+	private int Frames = 0;
+		
+	public Transform TheBall;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = transform.forward * 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frames++;
+		if (Frames == 6){
+			Frames = 0;
+			BallDmg--;
+		}
+	}
+	
+	void OnCollisionEnter (Collision other) {
+		Instantiate (TheBall, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
