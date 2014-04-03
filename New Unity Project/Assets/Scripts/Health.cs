08d17bd125a73af4a63e7151021c76316ecdbe2e
kyhnmette <kyhnmette@gmail.com>
2014-04-03 19:05:35 +0200

    still working on health script

--- New Unity Project/Assets/Scripts/Health.cs
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -1,25 +1,30 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class Health : MonoBehaviour {
-
-	public gameObject healthBar;
-	public gameObject larsObject;
+	/*
+	public GameObject healthBar;
+	public GameObject larsObject;
 	Lars lars;
 
 	// Use this for initialization
 	void Start () {
-		lars = gameObject.Find("Lars").GetComponent<Lars>();
+		lars = GameObject.Find("Lars").GetComponent<Lars>();
 
-		gameObject healthBar = (gameObject)Instantiate (new Vector3 (0, 0, 0));
+		GameObject healthBar = (GameObject)Instantiate(Resources.Load("nontexturedAPPLE"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (lars.HP <= 0) {
+		if (lars.HP() <= 0) {
 			destroy (gameObject);
 		}
-		transform.localScale = new Vector3(transform.localScale.y, lars.HP);
+		transform.localScale = new Vector3(transform.localScale.y, lars.HP());
 
-	}
+<<<<<<< HEAD
+	}*/
 }
+=======
+	}
+}*/
+>>>>>>> FETCH_HEAD
