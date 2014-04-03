2ac62981c5adfacf6cc438c8f06df05cb0edff6d
kyhnmette <kyhnmette@gmail.com>
2014-04-03 13:20:49 +0200

    tree root
    tree root script

---
+++ New Unity Project/Assets/Scripts/TreeRoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeRoot : MonoBehaviour {
+
+	//public gameObject treeObject;
+	//tree treeScript;
+
+	// Use this for initialization
+	void Start () {
+		//treeScript = gameObject.Find("tree").GetComponent<tree>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.name == "Lars") {
+			//treeScript.coHappen = true;
+			Destroy (gameObject);
+
+
+
+		}
+		
+	}
+
+}
