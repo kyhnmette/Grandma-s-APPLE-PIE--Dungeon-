ff9996b046b4a8362d55210127dfb8a1a7a0c922
Steffen Pihl <spihl12@student.aau.dk>
2014-04-03 16:56:47 +0200

    - Ball of fire now hitting the apples - Now the game can't be played before going tho the intro and "How to play" - Lose GUI added into the GUI scrpt, when Lars's HP becomes 0 or below will it pop up with the Lose screen (which still needs to be done before inplementing)
    Only bug is that when a BallOfFire is sent off will it not stop when
    pausing the game. It shall be discussed tomorrow if that is nessary to
    fix or is just a "nice to have"

--- New Unity Project/Assets/Scripts/GUI.cs
+++ New Unity Project/Assets/Scripts/GUI.cs
@@ -3,45 +3,69 @@ using System.Collections;
 
 public class GUI : MonoBehaviour {
 	
-	private bool IntroMenuOn {get; set;}
-	private bool HowToMenuOn {get; set;}
+	private bool IntroMenuOn;
+	private bool HowToMenuOn;
+	private static bool GameOn;
 	
 	public GUITexture IntroImage;
 	public GUITexture HowToImage;
+	public GUITexture Win;
+	public GUITexture Lose;
+	
+	public static bool GetGameOn () {
+		return GameOn;
+	}
 	
 	// Use this for initialization
 	void Start () {
 		IntroMenuOn = true; // The intro menu comes first so it will be set to true right away
 		HowToMenuOn = false;
+		GameOn = false;
+		Win.enabled = false;
+		Lose.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.KeypadEnter)){
+			// This section handles the intro and How to play scenes, click Enter on the keypad to continue
 			if (IntroMenuOn == true){
 				IntroMenuOn = false;
 				HowToMenuOn = true;
 			}
 			else if (HowToMenuOn == true){
 				HowToMenuOn = false;
+				GameOn = true;
 			}
 		}
 		
-		if (IntroMenuOn == true){
+		if (Input.GetKeyDown (KeyCode.P)){
+			// This section functions as the pause button, click on P to "turn off" the game mid-game and P again to "turn on" the game
+			if (GameOn == true)
+				GameOn = false;
+			else if (GameOn == false)
+				GameOn = true;
+		}
+		
+		if (IntroMenuOn == true && GameOn == false){
 			// tells us what shall be seen when the "Intro" menu is on using the enabled keyword (shall it be shown or not?)
 			IntroImage.enabled = true;
 			HowToImage.enabled = false;
 		}
 		
-		else if (HowToMenuOn == true){
+		else if (HowToMenuOn == true && GameOn == false){
 			// tells us what shall be seen when the "How to Play" menu is on using the enabled keyword (shall it be shown or not?)
 			IntroImage.enabled = false;
 			HowToImage.enabled = true;
 		}
-		else {
+		else if (GameOn == true){
+			// tells us what shall be seen when the game is running
 			IntroImage.enabled = false;
 			HowToImage.enabled = false;
 		}
+		
+		if (Lars.GetLarsHP() <= 0)
+			Lose.enabled = true;
 	}
 	
 	void OnGUI () {
