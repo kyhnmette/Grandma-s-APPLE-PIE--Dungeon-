6eca22c59237e8528e5d8af24592c6018a5e1d89
kyhnmette <kyhnmette@gmail.com>
2014-04-03 13:20:26 +0200

    health bar script
    i have tried to make the health bar script

---
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public gameObject healthBar;
+	public gameObject larsObject;
+	Lars lars;
+
+	// Use this for initialization
+	void Start () {
+		lars = gameObject.Find("Lars").GetComponent<Lars>();
+
+		gameObject healthBar = (gameObject)Instantiate (new Vector3 (0, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lars.HP <= 0) {
+			destroy (gameObject);
+		}
+		transform.localScale = new Vector3(transform.localScale.y, lars.HP);
+
+	}
+}
