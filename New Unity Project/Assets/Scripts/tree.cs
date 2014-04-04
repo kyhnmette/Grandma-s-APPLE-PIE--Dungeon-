e1216cf73bf2664d9fc57e28645935554a78acf5
LisbethRasmussen <llra12@student.aau.dk>
2014-04-04 13:44:47 +0200

    uncommenting tree
    Yep, that's what was done.

--- New Unity Project/Assets/Scripts/tree.cs
+++ New Unity Project/Assets/Scripts/tree.cs
@@ -17,15 +17,15 @@ public class tree : Life {
 
 		// code to put the tree object into the scene needs to be done
 
-		life.HP = 200; // (temp number) need to set the different numbers
-		life.ATK = 40; //
+		HP = 200; // (temp number) need to set the different numbers
+		ATK = 40; //
 
 		lars = GameObject.Find("Lars");
 	}
 	
 	// Update is called once per frame
 	 void Update () {
-		valueX = lars.position.x -= tree.position.x; // x-axis distance from the tree and lars 
+		/*valueX = lars.position.x -= tree.position.x; // x-axis distance from the tree and lars 
 		valueZ = lars.position.z -= tree.position.z; // z-axis distance from the tree and lars
 
 		if (valueX < 0 && valueX > 1 && valueZ < 0 && valueZ > 1) {
@@ -52,16 +52,16 @@ public class tree : Life {
 			// attack 
 			larsOldPos = lars.transform.position;
 			Invoke ("damageFunction", 6);
-		}
+		}*/
 		
 		if (HP <= 0){
 			Destroy(gameObject);
 		}
 	} 
 
-	public void damageFunction () {
+	/*public void damageFunction () {
 		Instantiate (treeRoot, larsOldPos);
-	}
+	}*/
 
 	void OnCollisionEnter(Collision other) {
 		if (other.gameObject.name == "BallOfFire(Clone)"){ 
