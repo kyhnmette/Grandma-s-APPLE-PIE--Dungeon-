84f2469d54f27e2c432e5df1827e748996799ad6
kyhnmette <kyhnmette@gmail.com>
2014-04-04 11:56:39 +0200

    Get function for the 3 apple enemies
    GetATK and ATK assigned

--- New Unity Project/Assets/Scripts/AppleGREEN.cs
+++ New Unity Project/Assets/Scripts/AppleGREEN.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AppleGREEN : Apple{
+public class AppleGREEN : Apple {
+
+	public static short GetATK() {
+		return ATK;
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		ATK = 5;
 	}
 	
 	// Update is called once per frame
