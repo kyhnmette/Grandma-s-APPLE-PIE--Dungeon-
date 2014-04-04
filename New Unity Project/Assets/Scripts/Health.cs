9ec770dad3cb096b91fab98120f1c29671630d0a
kyhnmette <kyhnmette@gmail.com>
2014-04-04 11:05:18 +0200

    changes of health script
    health bar is placed on scene, but still needs to be tested

--- New Unity Project/Assets/Scripts/Health.cs
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -4,24 +4,23 @@ using System.Collections;
 public class Health : MonoBehaviour {
 
 	public GameObject healthBar;
-	public GameObject larsObject;
-	Lars lars;
-	short hp;
+
 
 	// Use this for initialization
 	void Start () {
-		lars = GameObject.Find("Lars").GetComponent<Lars>();
 
-		GameObject healthBar = (GameObject)Instantiate(Resources.Load("nontexturedAPPLE"));
+
+		//GameObject healthBar = (GameObject)Instantiate(Resources.Load("nontexturedAPPLE"));
+		GameObject healthBarSwan = (GameObject)Instantiate(healthBar, new Vector3(5f,1f,10), transform.rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (hp <= 0) {
+		if (Lars.GetHP() <= 0) {
 			Destroy (gameObject);
 		}
-		transform.localScale = new Vector3(transform.localScale.y, hp);
+		transform.localScale = new Vector3(transform.localScale.y, Lars.GetHP());
 
 
 	}
