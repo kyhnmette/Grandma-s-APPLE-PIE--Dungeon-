7d54b8d37930c5457809d352d03e953d793f702c
Steffen Pihl <spihl12@student.aau.dk>
2014-04-05 14:30:40 +0200

    Tree and Root now fully working
    The tree is now spawned and can attack wtih its roots! Tho the system
    dosn't work as planned as the roots might appear random places thanks to
    some timers I had to use, BUT the random places have made me run into
    the roots a few times which makes it quite difficult and fun :3
    
    Also the "animation" on the roots are done which is a timer it itself...
    and I had to use Setters for that so we are now using that as well which
    is nice :3

--- New Unity Project/Assets/Scripts/TreeRoot.cs
+++ New Unity Project/Assets/Scripts/TreeRoot.cs
@@ -2,10 +2,33 @@
 using System.Collections;
 
 public class TreeRoot : Life {
-
-	//public gameObject treeObject;
-	//tree treeScript;
-
+	
+	private static float PosX;
+	private static float PosY;
+	private static float PosZ;
+	private static bool IsAttacking;
+	private bool IsMovingUp;
+	
+	public static void SetPosX (float NewPosX) {
+		PosX = NewPosX;
+	}
+	
+	public static bool GetIsAttacking () {
+		return IsAttacking;
+	}
+	
+	public static void SetIsAttacking (bool New) {
+		IsAttacking = New;
+	}
+	
+	/*public static void SetPosY (float NewPosY) {
+		PosY = NewPosY;
+	}*/
+	
+	public static void SetPosZ (float NewPosZ) {
+		PosZ = NewPosZ;
+	}
+	
 	public static int GetATK(){
 		return ATK; 
 	}
@@ -13,25 +36,36 @@ public class TreeRoot : Life {
 	// Use this for initialization
 	void Start () {
 		ATK = 20;
-
-		//treeScript = gameObject.Find("tree").GetComponent<tree>();
+		IsAttacking = false;
+		IsMovingUp = true;
+		
+		PosX = 11.0f;
+		PosY = -1.5f;
+		PosZ = 11.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-
-	void OnCollisionEnter(Collision other) {
-		if (other.gameObject.name == "Lars") {
-			//treeScript.coHappen = true;
-			Destroy (gameObject);
-
-
-
+		//print (IsMovingUp);
+		print (transform.position);
+		if (IsAttacking == false){
+			transform.position = new Vector3(PosX,PosY,PosZ);
+		}
+		else if (IsAttacking == true && IsMovingUp == true){
+			transform.position += new Vector3(0.0f,5.0f,0.0f)* Time.deltaTime;
+		}
+		
+		if (transform.position.y >= 1.0f){
+			IsMovingUp = false;
+		}
+		else if (transform.position.y <= -1.5f){
+			IsAttacking = false;
+			IsMovingUp = true;
+		}
+		
+		if (IsMovingUp == false){
+			transform.position += new Vector3(0.0f,-5.0f,0.0f)* Time.deltaTime;
 		}
 		
 	}
-
 }
