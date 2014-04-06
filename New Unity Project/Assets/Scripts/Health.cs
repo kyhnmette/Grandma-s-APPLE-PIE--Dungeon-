c843aafb8c0aa7595e034d6d9a3bd05100276ae7
kyhnmette <kyhnmette@gmail.com>
2014-04-06 17:20:21 +0200

    testing health bar with GUI
    trying to make health bar with GUI

--- New Unity Project/Assets/Scripts/Health.cs
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Health : MonoBehaviour {
 
 	public GameObject healthBar;
+	public GameObject hpBar;
 
 
 	// Use this for initialization
@@ -13,17 +14,24 @@ public class Health : MonoBehaviour {
 
 		//GameObject healthBar = (GameObject)Instantiate(Resources.Load("nontexturedAPPLE"));
 		GameObject.Instantiate(healthBar, new Vector3(5f,1f,10f), transform.rotation);
+		hpBar = GameObject.Find ("wallSubstitute(Clone)");
+		hpBar.transform.Rotate(0,0,90f);
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Lars.GetHP() <= 0) {
+		if (Lars.GetHeroHP() <= 0) {
 			Destroy (gameObject);
 		}
-		transform.localScale = new Vector3(transform.localScale.y, Lars.GetHP());
+		print (Lars.GetHeroHP());
+		//transform.localScale = new Vector3(transform.localScale.x, Lars.GetHeroHP(), transform.localScale.z);
 
+	}
 
+	void OnGUI() {
+		GUI.Box(new Rect(10, 10, 10, 10), "Hallo");
 	}
 }
