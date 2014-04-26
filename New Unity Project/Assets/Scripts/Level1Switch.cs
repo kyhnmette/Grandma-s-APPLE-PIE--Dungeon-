eedb66594edd9097cbdf9a490c2cb4fa06666886
kyhnmette <kyhnmette@gmail.com>
2014-04-26 23:35:18 +0200

    coloring portal

--- New Unity Project/Assets/Scripts/Level1Switch.cs
+++ New Unity Project/Assets/Scripts/Level1Switch.cs
@@ -5,12 +5,16 @@ public class Level1Switch : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		transform.position = new Vector3(3f, 2f, 3f);
+		transform.localScale = new Vector3(2.0F, 3.0F, 0.2F);
+		this.gameObject.renderer.material.color = new Color(2f, 2f, 213f);
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate(Vector3.up * Time.deltaTime * 100);
+
 	}
 
 	void OnCollisionEnter(Collision other){
--- New Unity Project/Assets/Scripts/Level2Switch.cs
+++ New Unity Project/Assets/Scripts/Level2Switch.cs
@@ -5,14 +5,18 @@ public class Level2Switch : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		transform.position = new Vector3(3f, 2f, 3f);
+		transform.localScale = new Vector3(2.0F, 3.0F, 0.2F);
+		this.gameObject.renderer.material.color = new Color(2f, 2f, 213f);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * 100);
+		
+	}
 	
-		}
-
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.name == "Lars") {
 			Destroy (gameObject);
