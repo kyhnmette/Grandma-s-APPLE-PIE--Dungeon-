f51b1401402dd4fe2babfd6ef9b0130457b39955
Steffen Pihl <spihl12@student.aau.dk>
2014-04-26 14:51:23 +0200

    Steffen's homework done! (Read notes details!)
    - GUI can not be seen.
    This one is almost finished! But Mette is probably working with the
    scenes so I will not finish the final details until nobody is working on
    them!
    
    - Tree shall be made solid
    I found out that the three IS sloid, BUT its collider box is very small
    and is INSIDE the three and not AROUND the tree (try to drag it out on
    the scene and click on it, then will you see what I mean)
    
    - You win / lose missing
    They have been added and just need to be added into every scene when no
    one is working on the scenes
    
    -Tree root ranged fixed
    The root's range have been doubled where its max range before was 5.0
    and now is 10.0 (numbers might be fixed when the three gets into its
    right position)
    
    - Lars shall die when touching the tree
    That have been done! But again shall he go INTO the three and and not
    just touch the model itself.

--- New Unity Project/Assets/Scripts/GUIinterface.cs
+++ New Unity Project/Assets/Scripts/GUIinterface.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class GUIinterface : MonoBehaviour {
 	
+	public GUITexture Win;
+	public GUITexture Lose;
 	public GUITexture AppleSeed;
 
 	// Use this for initialization
 	void Start () {
 		AppleSeed.enabled = false;
+		Win.enabled = false;
+		Lose.enabled = false;
+		
+		AppleSeed.pixelInset = new Rect (-Screen.width/2.5f, Screen.height/11, 170, 170);
+		Win.pixelInset = new Rect (-Screen.width/4, -Screen.height/2, 750, 750);
+		Lose.pixelInset = new Rect (-Screen.width/4, -Screen.height/2, 750, 750);
+		
+		tree.SetBossHP(1); // Just to avoid a bug with the win screen
 	}
 	
 	// Update is called once per frame
@@ -15,14 +25,18 @@ public class GUIinterface : MonoBehaviour {
 		if (GUIScript.GetGameOn() == true){
 			AppleSeed.enabled = true;
 		}
+		
+		if (Lars.GetHeroHP() <= 0)
+			Lose.enabled = true;
+		else if (tree.GetBossHP() <= 0)
+			Win.enabled = true;
 	}
 	
 	void OnGUI () {
 		// Here shall we insert the text labels for the game, example:
 		if (GUIScript.GetGameOn() == true){
-			GUI.Label(new Rect(1200, 125, 200, 100), "<size=40>" + Lars.GetPoints() + "</size>");
+			GUI.Label(new Rect(Screen.width/6.5f, Screen.height/4, 200, 100), "<size=40>" + Lars.GetPoints() + "</size>");
 			// This one will show the number of apple seeds the player have collected.
-			// The numbers inside of Rect means: #1 Position from left, #2 Position from top, #3 Width and #4 Height of the box
 		}
 	}
 }
